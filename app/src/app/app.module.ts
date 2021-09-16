02b23b1c76a2a98ae89b4ac041e5f5372436ecee
Luís Cunha <luiscunha1989@gmail.com>
2021-09-17 00:45:54 +0100

    -Updated angular version from 10 to 12 -Updated project dependencies -Added PWA support

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -12,7 +12,12 @@ import { environment } from '../environments/environment';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }), ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: environment.production,
+  // Register the ServiceWorker as soon as the app is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerImmediately'
+})],
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
