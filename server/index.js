2cb2de66ebca63d858ea9cfee47d8da977e8298b
Luís Cunha <luiscunha1989@gmail.com>
2021-09-17 22:31:10 +0100

    -Added missing CORS permission -Corrected socket.io server version

--- server/index.js
+++ server/index.js
@@ -1,28 +1,29 @@
 const PORT = process.env.PORT || 3000;
 const httpServer = require("http").createServer();
 const io = require("socket.io")(httpServer, {
-  cors: {
-    origin: "https://techsessions-demo.web.app",
-    methods: ["GET", "POST"]
-  }
+	cors: {
+		origin: ["https://techsessions-demo.web.app", "https://aquele-chat.web.app", "http://localhost:8100"],
+		methods: ["GET", "POST"]
+	}
 });
 
 io.on("connection", socket => {
-
 	socket.on("create-channel", channel => {
+		console.log("create-channel", channel);
 		socket.join(channel)
 	});
 
 	socket.on("join-channel", channel => {
-		socket.join(channel);
-		socket.emit("joined-channel", "you joined " + channel);
-		socket.broadcast.to(channel).emit("joined-channel", "new member joined");
+		console.log("join-channel", channel);
+		socket.join(channel.name);
+		socket.emit("joined-channel", "you joined " + channel.name);
+		socket.broadcast.to(channel.name).emit("joined-channel", channel.userName + " joined the channel");
 	});
 
 	socket.on("send-message", message => {
-		socket.broadcast.to(message.channel).emit("message", message.text);
+		console.log("send-message", message);
+		socket.broadcast.to(message.channel).emit("message", message);
 	});
-
 });
 
 httpServer.listen(PORT);
